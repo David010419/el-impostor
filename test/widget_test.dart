3891de75035458b45d3fb7a3cfa2dd562a6400a9
David010419 <dcastillo@global-retail.com>
2026-01-03 15:25:42 +0100

    Añadidos archivos para AdSense y políticas

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:el_impostor_game/main.dart';
+import 'package:the_impostor/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
